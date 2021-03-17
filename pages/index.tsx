27bf8abcfa7caa80a23f26a9f0973a7e5a347651
markkkkas <klapovsciuk@gmail.com>
2021-03-17 22:12:52 +0200

    Created Navigation component, fetch categories from database

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,43 @@
-export default function Home() {
-  return <h1>index</h1>;
+import { PrismaClient } from '@prisma/client';
+import Navigation, { NavItem } from '../components/Navigation';
+
+interface IHome {
+  navItems: NavItem[];
+}
+
+export default function Home({ navItems }: IHome) {
+  return <Navigation navItems={navItems} />;
+}
+
+export async function getStaticProps() {
+  const prisma = new PrismaClient();
+  const categories = await prisma.categories.findMany();
+
+  const navItems = [
+    {
+      label: 'Shuffle',
+      href: '#',
+    },
+    {
+      label: 'Trending',
+      href: '#',
+    },
+    {
+      label: 'Fresh',
+      href: '#',
+    },
+    {
+      label: 'Categories',
+      children: categories.map((item) => {
+        return {
+          label: item.name,
+          href: `/categories/${item.name.toLocaleLowerCase()}`,
+        };
+      }),
+    },
+  ];
+
+  return {
+    props: { navItems },
+  };
 }
