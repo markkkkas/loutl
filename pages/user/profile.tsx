2531ff9ed4cba14d1180d30735232dfe7d4477dd
markkkkas <klapovsciuk@gmail.com>
2021-03-18 23:27:07 +0200

    Redirect user if not authenticated and tries access authenticated-only routes

--- pages/user/profile.tsx
+++ pages/user/profile.tsx
@@ -1,10 +1,29 @@
+// next
+import { useRouter } from 'next/router';
+
+// next-auth
+import { useSession } from 'next-auth/client';
+
+// chakra
 import Navigation from '@/components/Navigation';
 
 export default function Profile() {
-  return (
-    <>
-      <Navigation />
-      <h1>User Profile Page</h1>
-    </>
-  );
+  const [session, loading] = useSession();
+  const router = useRouter();
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (session) {
+    return (
+      <>
+        <Navigation />
+        <h1>User Profile Page</h1>
+      </>
+    );
+  }
+
+  router.push('/');
+  return <h1>Loading...</h1>;
 }
